4a09039d9ad0cc1f0f7b3045f94f65e6fd00bb24
Charles Zhang <8474271+ZhangCharles@users.noreply.github.com>
2025-11-06 22:23:53 +0800

    可以通过bot进行搜索并下载

--- README.md
+++ README.md
@@ -13,29 +13,19 @@ docker build --tag wrapper .
 sudo docker run -v ./wrapper/rootfs/data:/app/rootfs/data -p 10020:10020 -p 20020:20020 -it -e args="-L username:password -H 0.0.0.0" wrapper
 ```
 
-# Build Downloader
-https://github.com/zhaarey/apple-music-downloader
+# Download AMDL
+https://github.com/ZhangCharles/apple-music-downloader
 
 ### [MP4Box](https://gpac.io/downloads/gpac-nightly-builds/) Must be Installed
-```
-git https://github.com/zhaarey/apple-music-downloader.git
-cd apple-music-downloader
-go build main.go
-```
 
 # Run
-### Copy Downloader
+### Move Downloader to Bot rootDir and Change Mode
 ```
-git https://github.com/ZhangCharles/applemusic-dl-bot.git
-cp ./applemusic-downloader/main ./applemusic-dl-bot/main
-cp ./applemusic-downloader/agent.js ./applemusic-dl-bot/agent.js
-cp ./applemusic-downloader/agent-arm64.js ./applemusic-dl-bot/agent-arm64.js
-cd applemusic-dl-bot
 chomd +x ./main
 ```
 ### Edit config
 ```
-cp example.env .env
+cp .env.example .env
 vi .env
 vi config.yaml
 ```
--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 import env
 from pyrogram import Client, filters, types
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from tinytag import TinyTag
 import subprocess
 import glob
+import requests
+from bs4 import BeautifulSoup
 from PIL import Image
 import io
 import os
+import uuid
+import shutil
+
+wait_list = []
+url_cache = {}
 
 app = Client('amdl-bot',api_id=env.API_ID, api_hash=env.API_HASH, bot_token=env.BOT_TOKEN)
+
+def download_music(url, outputDir):
+  cmd = f'./main --song -o {outputDir} {url}' if '?i=' in url else f'./main -o {outputDir} {url}'
+  subprocess.run(cmd, shell=True)
+  file_list = find_m4a_files(outputDir)
+  cover = find_cover_file(outputDir)[0] if find_cover_file(outputDir) else None
+  thumb = create_thumb(cover) if cover else None
+  audio_gourp = []
+  for m in file_list:
+    tag = get_metadata(m)
+    audio_gourp.append(
+      types.InputMediaAudio(
+        media=m,
+        duration=int(tag.duration),
+        performer=', '.join(tag.other.get('performer')) if tag.other.get('performer') else tag.artist,
+        title=tag.title,
+        thumb=thumb,
+        )
+      )
+  print("下载完成,正在上传")
+  return audio_gourp
 
 def get_metadata(file):
  tag: TinyTag = TinyTag.get(file)
@@ -28,47 +57,84 @@ def create_thumb(image_path):
   bio.seek(0)
   return bio
 
-@app.on_message(filters.command('amdl'))
-async def amdl(client, message: types.Message):
+@app.on_message(filters.command('search'))
+async def search(client: Client, message: types.Message):
   if str(message.chat.id) in env.CHAT_ID:
     args = message.command
-    if args[1]:
+    if args and len(args)>1:
+      text = ' '.join(args[1:])
+      resp = requests.get(f'https://music.apple.com/cn/search?term={text}')
+      resp.encoding = 'utf-8'
+      soup = BeautifulSoup(resp.text, 'html.parser')
+      results = soup.find_all(attrs={'data-testid':'grid-item'})
+      buttons = []
+      url_cache.clear()
+      for item in results:
+        i = item.find(attrs={'data-testid':'click-action'})
+        label = i['aria-label']
+        title = label.split('·')[0].strip()
+        category = label.split('·')[1].strip()
+        url = i['href']
+        if '艺人' not in category:
+          key = str(uuid.uuid4())[:8]
+          url_cache[key] = {"title":title, "url": url}
+          buttons.append([InlineKeyboardButton(text=label, callback_data=f'select:{message.from_user.id}:{key}')])
+      await message.reply(
+        "请选择需要下载的歌曲/专辑",
+        reply_markup=InlineKeyboardMarkup(buttons)
+      )
+
+@app.on_callback_query(filters.regex(r"^select:(\d+):(.+)$"))
+async def on_select(client: Client, callback_query: types.CallbackQuery):
+  user_id_str, key = callback_query.data.split(":")[1:]
+  info = url_cache.get(key, None)
+  if info:
+    title = info["title"]
+    url = info["url"]
+  else:
+    await callback_query.message.edit_text(
+        f"搜索已过期"
+      )
+    return
+  if callback_query.from_user.id != int(user_id_str):
+    await callback_query.answer(
+      "这不是你的搜索结果",
+      show_alert=True
+    )
+  else:
+    try:
+      id = callback_query.id
+      outputDir = f'./downloads/{id}'
+      await callback_query.message.edit_text(
+        f"正在下载：{title}"
+      )
+      audio_gourp = download_music(url, outputDir)
+      for i in range(0, len(audio_gourp), 10):
+        batch = audio_gourp[i:i + 10]
+        await callback_query.message.reply_media_group(media=batch)
+      shutil.rmtree(outputDir, ignore_errors=True)
+      print("已上传")
+    except Exception as e:
+      await callback_query.message.reply(text='下载失败')
+      print(e)
+
+@app.on_message(filters.command('amdl'))
+async def amdl(client: Client, message: types.Message):
+  if str(message.chat.id) in env.CHAT_ID:
+    args = message.command
+    if args and len(args) > 1:
       text = ' '.join(args[1:])
       if 'music.apple.com/cn/' in text:
         try:
-          cmd = f'./main {text}'
-          __path__ = './downloads'
-          subprocess.run(cmd, shell=True)
-          file_list = find_m4a_files(__path__)
-          cover = find_cover_file(__path__)[0] if find_cover_file(__path__) else None
-          thumb = create_thumb(cover) if cover else None
-          if file_list:
-            audio_gourp = []
-            for m in file_list:
-              tag = get_metadata(m)
-              audio_gourp.append(
-                types.InputMediaAudio(
-                  media=m,
-                  duration=int(tag.duration),
-                  performer=', '.join(tag.other.get('performer')) if tag.other.get('performer') else tag.artist,
-                  title=tag.title,
-                  thumb=thumb,
-                )
-              )
+          id = message.id
+          outputDir = f'./downloads/{id}'
+          audio_gourp = download_music(text, outputDir)
             #Media Group Limit 1-10
-            for i in range(0, len(audio_gourp), 10):
-              batch = audio_gourp[i:i + 10]
-              msg = await message.reply_media_group(media=batch)
-              #转发到频道
-              if env.CHANNEL:
-                ids = []
-                for m in msg:
-                  ids.append(m.id)
-                await app.forward_messages(chat_id=env.CHANNEL, from_chat_id=message.chat.id, message_ids=ids)
-            for f in file_list:
-              os.remove(f)
-            if cover:
-              os.remove(cover)
+          for i in range(0, len(audio_gourp), 10):
+            batch = audio_gourp[i:i + 10]
+            await message.reply_media_group(media=batch)
+          shutil.rmtree(outputDir)
+          print("已上传")
         except Exception as e:
           await message.reply(text='下载失败')
           print(e)
